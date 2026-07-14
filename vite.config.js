8934a5ee256802c3b2dfb0b59182501c86213c78
RafaPieper <pieper.rafael@gmail.com>
2026-07-14 11:48:29 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Port-Terminal-Simulator-Queueing/', // <-- Exact match of your repo name
+  base: '/Port-Terminal-Simulator-Queueing/', // <-- THIS IS THE MAGIC LINE
 })
